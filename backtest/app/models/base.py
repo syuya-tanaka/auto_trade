1af14915288e50175f7673e6f9fabea49f77dc4c
syuya-tanaka <84017791+syuya-tanaka@users.noreply.github.com>
2023-07-10 02:26:02 +0900

    DDL

--- backtest/app/models/base.py
+++ backtest/app/models/base.py
@@ -1,5 +1,7 @@
 """Database definition."""
+from contextlib import contextmanager
 import logging.config
+import threading
 
 from app.settings import Base
 from app.settings import engine
@@ -10,9 +12,41 @@ from app.settings import LOGGING_CONFIG
 logging.config.dictConfig(LOGGING_CONFIG)
 logger = logging.getLogger('models_base')
 
+lock = threading.Lock()
 
 def init_db() -> None:
-    Base.metadata.create_all(engine)
+    from app.models import candlesticks
+    Base.metadata.create_all(bind=engine)
+    logger.debug({
+        'action': 'create_table',
+        'status': 'success'
+    })
+
+
+def delete_db() -> None:
+    from app.models import candlesticks
+    Base.metadata.drop_all(bind=engine)
+    logger.debug({
+        'action': 'delete_table',
+        'status': 'success'
+    })
+
+
+@contextmanager
+def session_scope(session):
+    session = session.Session()
+    lock.acquire()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise Exception('It did not work.')
+    finally:
+        session.close()
+        lock.release()
 
 
 init_db()
+# delete_db()
+
